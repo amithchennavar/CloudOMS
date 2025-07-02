d6ed2f94802f2006f76471295334174457f83047
amithchennavar <amcr18ec@cmrit.ac.in>
2025-07-03 00:34:30 +0530

    Initial Stage with OrderSercvice API

---
+++ src/OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        public static List<Order> _orders = new List<Order>();
+        private static int _nextId = 1;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> GetOrders()
+        {
+            return Ok(_orders);
+        }
+
+        [HttpPost]
+        public ActionResult<Order> CreateOrder([FromBody] Order order)
+        {
+            if (order == null)
+            {
+                return BadRequest("Order data is required");
+            }
+            order.Id = _nextId++;
+            order.OrderDate = DateTime.UtcNow;
+            _orders.Add(order);
+            return CreatedAtAction(nameof(GetOrders), new { id = order.Id }, order);
+
+        }
+    }
+}
